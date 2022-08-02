16276c3755f95a53b11d8770817bcfb8ae5d6068
Muhammad Adhipatiunus <32333495+adhipatiunus@users.noreply.github.com>
2022-08-02 16:20:39 +0700

    updated with numba

--- LSMPS.py
+++ LSMPS.py
@@ -6,8 +6,10 @@ Created on Sat Jul 23 19:27:54 2022
 @author: adhipatiunus
 """
 import numpy as np
+import numba as nb
 from scipy import sparse
 
+@nb.njit
 def calculate_weight(r_ij, R_e):
     if r_ij < R_e:
         w_ij = (1 - r_ij / R_e)**2
@@ -15,49 +17,52 @@ def calculate_weight(r_ij, R_e):
         w_ij = 0
     return w_ij
 
-def LSMPSb(node_x, node_y, index, R_e, R_s, neighbor_list):
+@nb.njit
+def LSMPSb(node_x, node_y, index, Rmax, R, r_e, R_s, neighbor_matrix, n_neighbor):
     N = len(node_x)
-    b_data = [np.array([])] * N
-    EtaDx   = np.zeros((N,N))
-    EtaDy   = np.zeros((N,N))
-    EtaDxx  = np.zeros((N,N))
-    EtaDxy  = np.zeros((N,N))
-    EtaDyy  = np.zeros((N,N))
+    EtaDx   = np.zeros((N,N), dtype=np.float64)
+    EtaDy   = np.zeros((N,N), dtype=np.float64)
+    EtaDxx  = np.zeros((N,N), dtype=np.float64)
+    EtaDxy  = np.zeros((N,N), dtype=np.float64)
+    EtaDyy  = np.zeros((N,N), dtype=np.float64)
 
     for i in index:
-        H_rs = np.zeros((6,6))
-        M = np.zeros((6,6))
-        P = np.zeros((6,1))
-        b_temp = [np.array([])] * len(neighbor_list[i])
+        H_rs = np.zeros((6,6), dtype=np.float64)
+        M = np.zeros((6,6), dtype=np.float64)
+        P = np.zeros((6,1), dtype=np.float64)
+        b_temp = np.zeros((n_neighbor[i], 6, 1), dtype=np.float64)
         
        # print('Calculating derivative for particle ' + str(i) + '/' + str(N))
         
-        neighbor_idx = np.array(neighbor_list[i])
+        neighbor_idx = neighbor_matrix[i]
         
         #R_max = np.max(R[neighbor_idx])
         
         idx_i = i
         x_i = node_x[idx_i]
         y_i = node_y[idx_i]
+        R_e = r_e * Rmax[idx_i]
+        R_i = R[idx_i]
                 
         H_rs[0, 0] = 1
-        H_rs[1, 1] = R_s**-1
-        H_rs[2, 2] = R_s**-1
-        H_rs[3, 3] = 2 * R_s**-2
-        H_rs[4, 4] = R_s**-2
-        H_rs[5, 5] = 2 * R_s**-2
+        H_rs[1, 1] = R_s[i]**-1
+        H_rs[2, 2] = R_s[i]**-1
+        H_rs[3, 3] = 2 * R_s[i]**-2
+        H_rs[4, 4] = R_s[i]**-2
+        H_rs[5, 5] = 2 * R_s[i]**-2
                 
-        for j in range(len(neighbor_idx)):
+        for j in range(n_neighbor[i]):
             idx_j = neighbor_idx[j]
             x_j = node_x[idx_j]
             y_j = node_y[idx_j]
+            R_j = R[idx_j]
             
             x_ij = x_j - x_i
             y_ij = y_j - y_i
             r_ij = np.sqrt((x_ij)**2 + (y_ij)**2)
              
-            p_x = x_ij / R_s
-            p_y = y_ij / R_s
+            p_x = x_ij / R_s[i]
+            p_y = y_ij / R_s[i]
             
             P[0, 0] = 1.0
             P[1, 0] = p_x
@@ -66,66 +71,73 @@ def LSMPSb(node_x, node_y, index, R_e, R_s, neighbor_list):
             P[4, 0] = p_x * p_y
             P[5, 0] = p_y**2
             
-            w_ij = calculate_weight(r_ij, R_e)
-            M += w_ij * np.matmul(P, P.T)
+            w_ij = (R_j / R_i)**2 * calculate_weight(r_ij, R_e)
+            M += w_ij * np.dot(P, P.T)
             b_temp[j] = w_ij * P
         M_inv = np.linalg.inv(M)
-        MinvHrs = np.matmul(H_rs, M_inv)
-        b_data[i] = b_temp
+        MinvHrs = np.dot(H_rs, M_inv)
         
-        for j in range(len(neighbor_idx)):
+        for j in range(n_neighbor[i]):
             idx_j = neighbor_idx[j]
             #i[indexdx_i].append(idx_j)
-            Eta = np.matmul(MinvHrs, b_data[i][j])
-            EtaDx[idx_i,idx_j] = Eta[1]
-            EtaDy[idx_i,idx_j] = Eta[2]
-            EtaDxx[idx_i,idx_j] = Eta[3]
-            EtaDxy[idx_i,idx_j] = Eta[4]
-            EtaDyy[idx_i,idx_j] = Eta[5]
+            Eta = np.dot(MinvHrs, b_temp[j])
+            #print(Eta)
+            EtaDx[idx_i,idx_j] = Eta[1,0]
+            EtaDy[idx_i,idx_j] = Eta[2,0]
+            EtaDxx[idx_i,idx_j] = Eta[3,0]
+            EtaDxy[idx_i,idx_j] = Eta[4,0]
+            EtaDyy[idx_i,idx_j] = Eta[5,0]
             
     return EtaDx, EtaDy, EtaDxx, EtaDxy, EtaDyy
 
-def LSMPSbUpwind(node_x, node_y, index, R_e, R_s, neighbor_list, fx, fy):
+@nb.njit
+def LSMPSbUpwind(node_x, node_y, index, Rmax, R, r_e, R_s, neighbor_matrix, n_neighbor, fx, fy):
     N = len(node_x)
-    b_data = [np.array([])] * N
-    EtaDx   = np.zeros((N,N))
-    EtaDy   = np.zeros((N,N))
-    EtaDxx  = np.zeros((N,N))
-    EtaDxy  = np.zeros((N,N))
-    EtaDyy  = np.zeros((N,N))
-    
+    EtaDx   = np.zeros((N,N), dtype=np.float64)
+    EtaDy   = np.zeros((N,N), dtype=np.float64)
+    EtaDxx  = np.zeros((N,N), dtype=np.float64)
+    EtaDxy  = np.zeros((N,N), dtype=np.float64)
+    EtaDyy  = np.zeros((N,N), dtype=np.float64)
+
     for i in index:
-        H_rs = np.zeros((6,6))
-        M = np.zeros((6,6))
-        P = np.zeros((6,1))
-        b_temp = [np.array([])] * len(neighbor_list[i])
+        H_rs = np.zeros((6,6), dtype=np.float64)
+        M = np.zeros((6,6), dtype=np.float64)
+        P = np.zeros((6,1), dtype=np.float64)
+        b_temp = np.zeros((n_neighbor[i], 6, 1), dtype=np.float64)
         
-        #print('Calculating derivative for particle ' + str(i) + '/' + str(N))
+       # print('Calculating derivative for particle ' + str(i) + '/' + str(N))
         
-        neighbor_idx = np.array(neighbor_list[i])
-        ignored_neighbor = []
+        neighbor_idx = neighbor_matrix[i]
+        
+        #R_max = np.max(R[neighbor_idx])
         
         idx_i = i
         x_i = node_x[idx_i]
         y_i = node_y[idx_i]
-        fx_i = fx[i]
-        fy_i = fy[i]
+        R_e = r_e * Rmax[idx_i]
+        R_i = R[idx_i]
                 
         H_rs[0, 0] = 1
-        H_rs[1, 1] = R_s**-1
-        H_rs[2, 2] = R_s**-1
-        H_rs[3, 3] = 2 * R_s**-2
-        H_rs[4, 4] = R_s**-2
-        H_rs[5, 5] = 2 * R_s**-2
+        H_rs[1, 1] = R_s[i]**-1
+        H_rs[2, 2] = R_s[i]**-1
+        H_rs[3, 3] = 2 * R_s[i]**-2
+        H_rs[4, 4] = R_s[i]**-2
+        H_rs[5, 5] = 2 * R_s[i]**-2
                 
-        for j in range(len(neighbor_idx)):
+        for j in range(n_neighbor[i]):
             idx_j = neighbor_idx[j]
             x_j = node_x[idx_j]
             y_j = node_y[idx_j]
+            R_j = R[idx_j]
             
             x_ij = x_j - x_i
             y_ij = y_j - y_i
+            
+            fx_i = fx[i]
+            fy_i = fy[i]
+            
             r_ij = np.sqrt((x_ij)**2 + (y_ij)**2)
+            
             if r_ij == 0:
                 n_ij = np.array([0,0])
             else:
@@ -145,11 +157,10 @@ def LSMPSbUpwind(node_x, node_y, index, R_e, R_s, neighbor_list, fx, fy):
             if n_ij @ n_upwind > 0:
                 w_ij = calculate_weight(r_ij, R_e)
             else:
-                ignored_neighbor.append(idx_j)
                 w_ij = 1e-12
-            #print(w_ij)
-            p_x = x_ij / R_s
-            p_y = y_ij / R_s
+             
+            p_x = x_ij / R_s[i]
+            p_y = y_ij / R_s[i]
             
             P[0, 0] = 1.0
             P[1, 0] = p_x
@@ -158,86 +169,22 @@ def LSMPSbUpwind(node_x, node_y, index, R_e, R_s, neighbor_list, fx, fy):
             P[4, 0] = p_x * p_y
             P[5, 0] = p_y**2
             
-            M += w_ij * np.matmul(P, P.T)
+            w_ij = (R_j / R_i)**2 * calculate_weight(r_ij, R_e)
+            M += w_ij * np.dot(P, P.T)
             b_temp[j] = w_ij * P
-        #if np.linalg.det(M) < 1e-6:
-            """
-            for j in ignored_neighbor:
-                idx_j = j
-                #print(idx_j)
-                x_j = node_x[idx_j]
-                y_j = node_y[idx_j]
-                    
-                x_ij = x_j - x_i
-                y_ij = y_j - y_i
-                r_ij = np.sqrt((x_ij)**2 + (y_ij)**2)
-                    
-                p_x = x_ij / R_s
-                p_y = y_ij / R_s
-                    
-                P[0, 0] = 1.0
-                P[1, 0] = p_x
-                P[2, 0] = p_y
-                P[3, 0] = p_x**2
-                P[4, 0] = p_x * p_y
-                P[5, 0] = p_y**2
-                
-                w_ij = calculate_weight(r_ij, R_e)
-                    
-                M += w_ij * np.matmul(P, P.T)
-            """
-            """
-            h = 0.00625
-            x = np.linspace(node_x[i] - h, node_x[i] + h, 3)
-            y = np.linspace(node_y[i] - h, node_y[i] + h, 3)
-            x_s, y_s = np.meshgrid(x, y)
-            x_s = x_s.flatten()
-            y_s = y_s.flatten()
-            for j in range(len(x_s)):
-                idx_j = j
-                #print(idx_j)
-                x_j = x_s[idx_j]
-                y_j = y_s[idx_j]
-                    
-                x_ij = x_j - x_i
-                y_ij = y_j - y_i
-                r_ij = np.sqrt((x_ij)**2 + (y_ij)**2)
-                    
-                p_x = x_ij / R_s
-                p_y = y_ij / R_s
-                    
-                P[0, 0] = 1.0
-                P[1, 0] = p_x
-                P[2, 0] = p_y
-                P[3, 0] = p_x**2
-                P[4, 0] = p_x * p_y
-                P[5, 0] = p_y**2
-                
-                w_ij = calculate_weight(r_ij, R_e)
-                    
-                M += w_ij * np.matmul(P, P.T)
-            """
-        #print(M)
         M_inv = np.linalg.inv(M)
-        MinvHrs = np.matmul(H_rs, M_inv)
-        b_data[i] = b_temp
-        #print(b_temp)
+        MinvHrs = np.dot(H_rs, M_inv)
         
-        for j in range(len(neighbor_idx)):
+        for j in range(n_neighbor[i]):
             idx_j = neighbor_idx[j]
-            """
-            if idx_j in ignored_neighbor:
-                Eta = np.array([0, 0, 0, 0, 0, 0])
-            else:
-                Eta = np.matmul(MinvHrs, b_data[i][j])
-            """
-            Eta = np.matmul(MinvHrs, b_data[i][j])
             #i[indexdx_i].append(idx_j)
-            EtaDx[idx_i,idx_j] = Eta[1]
-            EtaDy[idx_i,idx_j] = Eta[2]
-            EtaDxx[idx_i,idx_j] = Eta[3]
-            EtaDxy[idx_i,idx_j] = Eta[4]
-            EtaDyy[idx_i,idx_j] = Eta[5]      
-    
+            Eta = np.dot(MinvHrs, b_temp[j])
+            #print(Eta)
+            EtaDx[idx_i,idx_j] = Eta[1,0]
+            EtaDy[idx_i,idx_j] = Eta[2,0]
+            EtaDxx[idx_i,idx_j] = Eta[3,0]
+            EtaDxy[idx_i,idx_j] = Eta[4,0]
+            EtaDyy[idx_i,idx_j] = Eta[5,0]
+            
     return EtaDx, EtaDy, EtaDxx, EtaDxy, EtaDyy
         
